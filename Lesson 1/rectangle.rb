19c7e8a628d468b48654f21a6a2e2f57a43af4f6
Александр Русев <aleksandrrusev@MacBook-Pro-Aleksandr.local>
2018-05-01 17:07:05 +0300

    follow review 2

--- Lesson 1/rectangle.rb
+++ Lesson 1/rectangle.rb
@@ -8,12 +8,13 @@ print "What is \"c\" side of triangle: "
 c = gets.to_f
 
 x = [a, b, c].sort
-
-if (x[2] ** 2) == (x[0] ** 2 + x[1] ** 2)
-elsif a == b && b == c
-  puts "It is an equilateral triangle."
-elsif a == b || a == c || b == c
-  print "It is a rectangular and isosceles triangle."
+rect = (x[2] ** 2).round(1) == (x[0] ** 2 + x[1] ** 2).round(1)
+if rect && x[0] == x[1]
+  print "It is a rectangular and isosceles triangle.\n" # прямоугольный и равнобедренный
+elsif rect
+  print "It is an rectangular triangle.\n" # прямоугольный
+elsif x[2] == x[0] && x[2] == x[1] 
+  print "It is an equilateral triangle.\n" # равносторонний
 else
-  puts "It is not a rectangular triangle."
+  print "Ooops! Not rectangular triangle.\n"
 end
