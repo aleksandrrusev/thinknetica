08bf46ea1e04af8a138219ca5e96f25a5e8e1062
Alexander Rusev <aleksandr.rusev@gmail.com>
2018-07-09 19:01:06 +0300

    small fix after rename

--- Lesson 3/Route.rb
+++ Lesson 3/Route.rb
@@ -6,12 +6,12 @@ class Route
   end
 
   def station_add(value)
-    stations.include?(value) ? "This elem: #{value} already have" : station_list.insert(-2, elem) }
+    stations.include?(value) ? "This elem: #{value} already have" : stations.insert(-2, elem)
   end
 
   def station_delete(value)
     if stations.length >= 2
-      stations.include?(value) ? station_list.delete(elem) : "This elem: #{value} not found!"
+      stations.include?(value) ? stations.delete(elem) : "This elem: #{value} not found!"
     else
       print "You can't do that. Rout must have at least two station! "
     end
